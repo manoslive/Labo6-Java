0964c2b8fb123a280b44b8a564716fe7b16b15f2
ChuckCooper <shaun.11.cooper@gmail.com>
2015-04-09 19:01:45 -0400

    v1.0

--- src/Extract.java
+++ src/Extract.java
@@ -1,5 +1,17 @@
 /**
  * Created by Emmanuel on 2015-04-09.
  */
+
 public class Extract {
+    StringBuilder contentBuilder = new StringBuilder();
+    try {
+        BufferedReader in = new BufferedReader(new FileReader("mypage.html"));
+        String str;
+        while ((str = in.readLine()) != null) {
+            contentBuilder.append(str);
+        }
+        in.close();
+    } catch (IOException e) {
+    }
+    String content = contentBuilder.toString();
 }
