1ccc000e6c9a4c71f533b0143a572535913f252c
manoslive <ebeloin@hotmail.com>
2015-04-09 19:14:16 -0400

    entête

--- src/Extract.java
+++ src/Extract.java
@@ -4,29 +4,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-/**
- * Created by Emmanuel on 2015-04-09.
+/*
+    Fait par Emmanuel Beloin et Shaun Cooper
+    Le 09/04/15
  */
 
 public class Extract {
     static void main(String[] args) {
+
         StringBuilder contentBuilder = new StringBuilder();
         try {
             BufferedReader in = new BufferedReader(new FileReader("mypage.html"));
             String str;
             while ((str = in.readLine()) != null) {
                 contentBuilder.append(str);
-            }
+        }
             in.close();
-        } catch (
-                IOException e
-                )
-
+        } catch (IOException e)
         {
+
         }
 
         String content = contentBuilder.toString();
-    }
+}
 
     public void FileWriter(String text, String file)
     {
